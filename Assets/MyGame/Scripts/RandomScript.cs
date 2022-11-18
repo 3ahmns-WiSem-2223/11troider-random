3e07b7c30208324497f967e335199b140476eb6e
theoroiroi <114597785+theoroiroi@users.noreply.github.com>
2022-11-18 16:56:22 +0100

    Final Commit

---
+++ Assets/MyGame/Scripts/RandomScript.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class RandomScript : MonoBehaviour
+{
+    [SerializeField] Color farbe;
+
+    private void Start()
+    {
+        float zahl = Random.value;
+        Debug.Log(zahl);
+
+        transform.position = Random.insideUnitSphere * 3;
+
+        farbe = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f);
+        GetComponent<Renderer>().material.color = farbe;
+
+    }
+
+}
